50db0eb863b3084eff7745ec671a505601289b8d
Filippo Berto <berto.f@protonmail.com>
2018-08-02 18:58:57 +0200

    Better logging

--- noip.go
+++ noip.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	"net/http"
-	"os"
 	"fmt"
 	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
 	"strings"
 	"time"
-	"strconv"
-	)
+)
 
 func runUpdate() {
 	username := os.Getenv("USER_NAME")
@@ -28,7 +29,7 @@ func runUpdate() {
 
 	ip := os.Getenv("IP")
 	if ip == "" {
-		fmt.Println("WARNING: IP environment variable not set. Using external IP.")
+		log.Println("WARNING: IP environment variable not set. Using external IP.")
 		resp, err := http.Get("http://myexternalip.com/raw")
 		if err != nil {
 			panic(err)
@@ -40,19 +41,21 @@ func runUpdate() {
 		}
 		ip = strings.Replace(string(body), "\n", "", -1)
 	}
+	log.Printf("IP: %s", ip)
 
+	log.Println("Requesting IP update to NoIP API")
 	req, err := http.NewRequest("GET", fmt.Sprintf("https://%s:%s@dynupdate.no-ip.com/nic/update?hostname=%s&myip=%s", username, password, hostname, ip), nil)
 	if err != nil {
-		panic(err)
+		log.Panic(err)
 	}
 	req.Header.Add("User-Agent", fmt.Sprintf("Go NoIP DDNS Update Client by bertof from %s", os.Getenv("hostname")))
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		panic(err)
+		log.Panic(err)
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
-	fmt.Printf("Response: %s\n", string(body))
+	log.Printf("Response: %s", string(body))
 }
 
 func main() {
